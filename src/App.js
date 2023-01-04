22297028d3bfa41b60911e4b02346f046c8d1015
tom <tom31@hotmail.co.uk>
2023-01-04 01:40:12 -0500

    got mapping working local need to clean up

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import { MapContainer, TileLayer } from 'react-leaflet'
+import ReactLeafletKml from 'react-leaflet-kml';
 import './App.css';
 
-function App() {
+
+let foobar ="foobar"
+
+function  App() {
+    const [kmlText, setKmlText] = useState(null);
+    useEffect(() => {
+	async function getKml(){
+	    const kmlTxt = new DOMParser().parseFromString( await fetch("http://192.168.50.26/doc.kml"
+    ).then(response => response.text()), 'text/xml');
+	    setKmlText(kmlTxt);
+	}
+	foobar="blat";
+	getKml();
+    }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+	<header className="App-header">
+	    <div>{  foobar }</div>
+      <MapContainer className="map-container" zoom={15} center={[51.00, 0.00]} >
+       	 <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        {kmlText && <ReactLeafletKml kml={kmlText} />} 
+      </MapContainer>
       </header>
     </div>
   );
